ee60466724ae4f3a545208d37bec36bf5597fa47
Will Mahoney <will.mahoney3@gmail.com>
2024-02-26 09:12:56 +0000

    First commit

---
+++ components/Footer.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <footer className="bg-blue-500 p-4">
+            <h1>Footer</h1>
+        </footer>
+    )
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,7 @@
+export default () => {
+    return (
+        <header className="bg-green-900 p-4">
+            <h1>Navbar</h1>
+        </header>
+    )
+}
